4f18270ba286a5b9a62d9a4532d5171acca7b457
dgw <dgw@technobabbl.es>
2015-07-11 15:51:11 -0400

    UNO doesn't have a 0 card

--- unobot.py
+++ unobot.py
@@ -77,8 +77,8 @@ STRINGS = {
 
 class UnoBot:
     def __init__(self):
-        self.colored_card_nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8',
-                                  '9', 'R', 'S', 'D2']
+        self.colored_card_nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
+                                  'R', 'S', 'D2']
         self.special_scores = {'R': 20, 'S': 20, 'D2': 20, 'WD4': 50, 'W': 50}
         self.colors = 'RGBY'
         self.special_cards = ['W', 'WD4']
